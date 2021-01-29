423737d02c00ba6f47536423c763f2a3a2c7f737
Jade Lynn Masker <jadelynn@masker.family>
2021-01-29 13:26:06 -0500

    commit tree d10816f25ad59cef03920c2860c1765fdd83b553

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Your code here
+// map
+function mapToNegativize(sourceArray) {
+	const newArray = [];
+	for (let i in sourceArray) {
+		newArray[i] = -sourceArray[i];
+	}
+	return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+	return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+	const newArray = [];
+	for (let i in sourceArray) {
+		newArray[i] = sourceArray[i] * 2;
+	}
+	return newArray;
+}
+
+function mapToSquare(sourceArray) {
+	const newArray = [];
+	for (let i in sourceArray) {
+		newArray[i] = sourceArray[i] ** 2;
+	}
+	return newArray;
+}
+
+// reduce
+function reduceToTotal(sourceArray, newTotal=0) {
+	for (const e of sourceArray) {
+		newTotal += e;
+	}
+	return newTotal;
+}
+
+function reduceToAllTrue(sourceArray) {
+	for (const e of sourceArray) {
+		if (e) {
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+	for (const e of sourceArray) {
+		if (e) {
+			return true;
+		}
+	}
+	return false;
+}
